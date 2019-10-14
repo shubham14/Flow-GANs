95616c040dd8601b61338555f7b001762d511f1f
Shubham Dash <shubham.ddash@gmail.com>
2019-10-13 17:09:13 -0700

    Added functional TF model structure

--- scripts/tf2_tutorial.py
+++ scripts/tf2_tutorial.py
@@ -1,22 +1,103 @@
 from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Embedding
+from tensorflow.keras import Model
+from tqdm import tqdm
 
 mnist = tf.keras.datasets.mnist
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 X_train, X_test = X_train / 255.0, X_test / 255.0
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+# for adding channels
+X_train = X_train[..., tf.newaxis]
+X_test = X_test[..., tf.newaxis]
 
-model.compile(optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy'])
+train_ds = tf.data.Dataset.from_tensor_slices(
+    (X_train, y_train)).shuffle(10000).batch(32)
 
-model.fit(X_train, y_train, epochs=1)
-model.evaluate(X_test, y_test, verbose=1)
+test_ds = tf.data.Dataset.from_tensor_slices(
+    (X_test, y_test)).batch(32)
+
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d2(self.d1(x))
+        return x
+
+
+def trainModel(model, loss, optimizer):
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+@tf.function
+def train_step(images, labels, loss_object, model, train_loss, train_accuracy):
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+@tf.function
+def test_step(images, labels, loss_object, test_loss, test_accuracy):
+    predictions = model(images)
+    t_loss = loss_object(labels, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+    
+#training loop
+def train(EPOCHS, train_ds, test_ds):
+    for epoch in range(EPOCHS):
+        for images, labels in train_ds:
+            train_step(images, labels)
+
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
+
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print(template.format(epoch+1,
+                                train_loss.result(),
+                                train_accuracy.result()*100,
+                                test_loss.result(),
+                                test_accuracy.result()*100))
+
+        # Reset the metrics for the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
+
+if __name__ == "__main__":
+    model = MyModel()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam()
+    #     model = tf.keras.models.Sequential([
+    #     tf.keras.layers.Flatten(input_shape=(28, 28)),
+    #     tf.keras.layers.Dense(128, activation='relu'),
+    #     tf.keras.layers.Dropout(0.2),
+    #     tf.keras.layers.Dense(10, activation='softmax')
+    # ])
+
+    # model.compile(optimizer='adam',
+    #             loss='sparse_categorical_crossentropy',
+    #             metrics=['accuracy'])
+
+    # model.fit(X_train, y_train, epochs=1)
+    # model.evaluate(X_test, y_test, verbose=1)
+
